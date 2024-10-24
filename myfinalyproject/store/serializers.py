1124b7c04dab3fc961153a0be992cc65f19ac1c1
aitbek_312_ <askerovaitbek0607@gmail.com>
2024-10-24 15:58:10 +0600

    dgndgjd

---
+++ myfinalyproject/store/serializers.py
@@ -0,0 +1,141 @@
+from rest_framework import serializers
+from .models import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'age',
+                  'date_registered', 'phone_number', 'status']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(**validated_data)
+        return user
+
+    def to_representation(self, instance):
+        refresh = RefreshToken.for_user(instance)
+        return {
+            'user': {
+                'username': instance.usrname,
+                'email': instance.email,
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+
+        }
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Неверные учетные данные')
+
+    def to_representation(self, instance):
+        refresh = RefreshToken.for_user(instance)
+        return {
+            'user': {
+                'username': instance.username,
+                'email': instance.email,
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+
+        }
+
+
+class UserProfileSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+
+class UserProfileSimpleSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['first_name', 'last_name']
+
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_name']
+
+
+class ProductPhotosSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPhotos
+        fields = ['image']
+
+
+class RatingSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    created_date = serializers.DateTimeField(format='%Y-%m-%d %H:%M')
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ProductListSerializers(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
+    date = serializers.DateTimeField(format='%d-%m-%Y')
+
+    class Meta:
+        model = Product
+        fields = ['product_name', 'category', 'price', 'average_rating',
+                  'date']
+
+    def get_average_rating(self, obj):
+        return obj.get_average_rating()
+
+
+class ProductDetailSerializers(serializers.ModelSerializer):
+    category = CategorySerializers()
+    ratings = RatingSerializers(many=True, read_only=True)
+    reviews = ReviewSerializers(many=True, read_only=True)
+    product = ProductPhotosSerializers(many=True, read_only=True)
+    average_rating = serializers.SerializerMethodField()
+    date = serializers.DateTimeField(format='%d-%m-%Y ')
+    owner = UserProfileSimpleSerializers()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'product_name', 'category', 'price', 'description', 'category',
+                  'active', 'ratings', 'average_rating', 'product_video', 'reviews', 'product', 'date', 'owner']
+
+    def get_average_rating(self, obj):
+        return obj.get_average_rating()
+
+
+class CarItemSerializers(serializers.ModelSerializer):
+    product = ProductListSerializers(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True, source='product')
+
+    class Meta:
+        model = CarItem
+        fields = ['id', 'product', 'product_id', 'qyantity', 'get_total_price']
+
+
+class CartSerializers(serializers.ModelSerializer):
+    items = CarItemSerializers(read_only=True, many=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items', 'total_price']
+
+    def get_total_price(self, obj):
+        return obj.get_total_price()
